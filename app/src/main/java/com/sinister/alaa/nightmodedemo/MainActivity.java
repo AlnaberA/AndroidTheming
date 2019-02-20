6158c4fd3eeab9df01eaf91774726c73c514f66d
Alaa Al Naber <alnaberalaa@gmail.com>
2019-02-20 18:21:58 -0500

    starting work on main activity, completed layout.

--- app/src/main/java/com/sinister/alaa/nightmodedemo/MainActivity.java
+++ app/src/main/java/com/sinister/alaa/nightmodedemo/MainActivity.java
@@ -2,9 +2,10 @@ package com.sinister.alaa.nightmodedemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Switch themeSwitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
